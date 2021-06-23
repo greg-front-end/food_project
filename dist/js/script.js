40c7815ddb1f7a9e8690c6c6cf139369409d2132
Martin <martinstudyweb@gmail.com>
2021-06-23 12:18:35 +0300

    function for create html element for after send user forms

--- dist/js/script.js
+++ dist/js/script.js
@@ -92,8 +92,8 @@ window.addEventListener('DOMContentLoaded', () => {
     // Modals
 
     const modal = document.querySelector('.modal'),
-          modalBtn = document.querySelectorAll('[data-modal]'),
-          modalBtnClose = document.querySelectorAll('[data-close]');
+          modalBtn = document.querySelectorAll('[data-modal]');
+        //   modalBtnClose = document.querySelectorAll('[data-close]');
 
     // let modalStyle = window.getComputedStyle(modal).diplay;
     function openModal() {
@@ -114,13 +114,13 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    modalBtnClose.forEach(btn => {
-        btn.addEventListener('click', modalClose);
-    });
+    // modalBtnClose.forEach(btn => {
+    //     btn.addEventListener('click', modalClose);
+    // });
 
     // close modal if click out
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') === '') {
              modalClose();
         }
     });
@@ -202,11 +202,12 @@ window.addEventListener('DOMContentLoaded', () => {
     // AJAX work with back-end and forms
     const forms = document.querySelectorAll('form');
     const message = {
-        loading: 'Loading',
-        success: 'Thank you, we will soon call you',
+        loading: 'img/form/Spinner-3.gif',
+        success: 'Thank you, we will soon call you back',
         failure: 'Somthing is going wrong...'
     };
 
+    // using function postData for every forms on site
     forms.forEach(item => {
         postData(item);
     });
@@ -215,10 +216,14 @@ window.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+                padding: 10px;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
@@ -246,15 +251,37 @@ window.addEventListener('DOMContentLoaded', () => {
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     // console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success);
                     form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
                 } else {
-                    statusMessage.textContent = message.failure;
+                    showThanksModal(message.failure);
                 }
             });
         }); 
+    }
+
+    // This function create html element for after user send forms
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>x</div>
+                <div calss="modal__title">${message}</div>
+            </div>
+        `;
+        modal.append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            modalClose();
+        }, 4000);
     }
 });
