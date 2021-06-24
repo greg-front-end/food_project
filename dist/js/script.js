d44da829ab1f58bdda56c93d99c6989df9b1680d
Martin <martinstudyweb@gmail.com>
2021-06-24 23:41:58 +0300

    change in style.css .menu class and .menu .container class

--- dist/js/script.js
+++ dist/js/script.js
@@ -232,6 +232,8 @@ window.addEventListener('DOMContentLoaded', () => {
     //     });
 
     // }
+
+
     // new MenuCards("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', '29', 75, '$', '.menu .container').render();
 
     // AJAX work with back-end and forms
